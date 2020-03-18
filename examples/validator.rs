43715ed098f53fdfe37b8d36218934a114f7a011
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2020-03-18 14:17:26 +0100

    validators wip, add custom errors
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- examples/validator.rs
+++ examples/validator.rs
@@ -1,9 +1,9 @@
 // #![feature(trait_alias)]
 use async_std::io;
 use async_std::task;
-use serde::{Deserialize, Serialize};
-use tide::{Middleware, Request, Next, Response};
 use futures::future::BoxFuture;
+use serde::{Deserialize, Serialize};
+use tide::{Middleware, Next, Request, Response};
 
 use std::collections::HashMap;
 use std::sync::Arc;
@@ -14,31 +14,51 @@ struct Cat {
 }
 
 #[inline]
-fn is_number(param_value: &str) -> Result<(), String> {
+fn is_number(param_value: &str) -> Result<(), CustomError> {
     if param_value.parse::<i64>().is_err() {
-        Err(format!("'{}' is not a valid number", param_value))
+        Err(CustomError {
+            status_code: 400,
+            message: format!("'{}' is not a valid number", param_value),
+        })
     } else {
         Ok(())
     }
 }
 
-
 #[inline]
-fn is_bool(param_value: &str) -> Result<(), String> {
+fn is_bool(param_value: &str) -> Result<(), CustomError> {
     match param_value {
         "true" | "false" => Ok(()),
-        other => Err(format!("'{}' is not a valid boolean", other))
+        other => Err(CustomError {
+            status_code: 400,
+            message: format!("'{}' is not a valid boolean", other),
+        }),
     }
 }
 
-fn is_length_under(min_length: usize) -> Box<dyn Fn(&str) -> Result<(), String> + Send + Sync + 'static> {
-    Box::new(move |param_value: &str| -> Result<(), String> {
+fn is_length_under(
+    min_length: usize,
+) -> Box<dyn Fn(&str) -> Result<(), CustomError> + Send + Sync + 'static> {
+    Box::new(move |param_value: &str| -> Result<(), CustomError> {
         if param_value.len() < min_length {
-            Err(format!("'{}' have not the minimal length of {}", param_value, min_length))
+            let my_error = CustomError {
+                status_code: 400,
+                message: format!(
+                    "'{}' have not the minimal length of {}",
+                    param_value, min_length
+                ),
+            };
+            Err(my_error)
         } else {
             Ok(())
         }
     })
+}
+
+#[derive(Debug, Serialize)]
+struct CustomError {
+    status_code: usize,
+    message: String,
 }
 
 fn main() -> io::Result<()> {
@@ -46,25 +66,23 @@ fn main() -> io::Result<()> {
         let mut app = tide::new();
 
         let mut validator_middleware = ValidatorMiddleware::new();
-        validator_middleware.insert_validator(ParameterType::Param("n"), is_number);
-        validator_middleware.insert_validator(ParameterType::QueryParam("test"), is_bool);
-        validator_middleware.insert_validator(ParameterType::QueryParam("test"), is_length_under(10));
+        validator_middleware.add_validator(ParameterType::Param("n"), is_number);
+        validator_middleware.add_validator(ParameterType::QueryParam("test"), is_bool);
+        validator_middleware.add_validator(ParameterType::QueryParam("test"), is_length_under(10));
 
-        app.at("/test/:n")
-            .middleware(validator_middleware)
-            .get(|_: tide::Request<()>| async move {
+        app.at("/test/:n").middleware(validator_middleware).get(
+            |_: tide::Request<()>| async move {
                 let cat = Cat {
                     name: "chashu".into(),
                 };
                 tide::Response::new(200).body_json(&cat).unwrap()
-            });
+            },
+        );
 
         app.listen("127.0.0.1:8080").await?;
         Ok(())
     })
 }
-
-// TODO: custom errors https://express-validator.github.io/docs/custom-error-messages.html with using closure to transform message into response error tide
 // TODO: add validation about cookies, headers and maybe body ? https://express-validator.github.io/docs/check-api.html
 // TODO: add ctx in closure to have other informations about request ? Maybe in further version
 // trait Validator = Fn(&str) -> Result<(), String> + Send + Sync + 'static;
@@ -72,38 +90,59 @@ fn main() -> io::Result<()> {
 #[derive(Debug, Clone, Hash, Eq, PartialEq)]
 enum ParameterType<'a> {
     Param(&'a str),
-    QueryParam(&'a str)
+    QueryParam(&'a str),
+    //Header
+    //Cookie
 }
 
-
-struct ValidatorMiddleware {
-    validators: HashMap<ParameterType<'static>, Vec<Arc<dyn Fn(&str) -> Result<(), String> + Send + Sync + 'static>>>
+struct ValidatorMiddleware<T>
+where
+    T: Serialize + Send + Sync + 'static,
+{
+    validators: HashMap<
+        ParameterType<'static>,
+        Vec<Arc<dyn Fn(&str) -> Result<(), T> + Send + Sync + 'static>>,
+    >,
 }
 
-impl ValidatorMiddleware {
-    pub fn new() -> ValidatorMiddleware {
+impl<T> ValidatorMiddleware<T>
+where
+    T: Serialize + Send + Sync + 'static,
+{
+    pub fn new() -> Self {
         ValidatorMiddleware {
             validators: HashMap::new(),
         }
     }
 
-    pub fn with_validators<F>(mut self, validators: HashMap<ParameterType<'static>, F>) -> Self where F: Fn(&str) -> Result<(), String> + Send + Sync + 'static {
+    pub fn with_validators<F>(mut self, validators: HashMap<ParameterType<'static>, F>) -> Self
+    where
+        F: Fn(&str) -> Result<(), T> + Send + Sync + 'static,
+    {
         for (param_name, validator) in validators {
-            self.insert_validator(param_name, validator);
+            self.add_validator(param_name, validator);
         }
         self
     }
 
-    pub fn insert_validator<F>(&mut self, param_name: ParameterType<'static>, validator: F) where F: Fn(&str) -> Result<(), String> + Send + Sync + 'static {
+    pub fn add_validator<F>(&mut self, param_name: ParameterType<'static>, validator: F)
+    where
+        F: Fn(&str) -> Result<(), T> + Send + Sync + 'static,
+    {
         let validator = Arc::new(validator);
         let validator_moved = Arc::clone(&validator);
-        self.validators.entry(param_name.into())
+        self.validators
+            .entry(param_name.into())
             .and_modify(|e| e.push(validator_moved))
             .or_insert(vec![validator]);
-    } 
+    }
 }
 
-impl<State> Middleware<State> for ValidatorMiddleware where State: Send + Sync + 'static {
+impl<State, T> Middleware<State> for ValidatorMiddleware<T>
+where
+    State: Send + Sync + 'static,
+    T: Serialize + Send + Sync + 'static,
+{
     fn handle<'a>(&'a self, ctx: Request<State>, next: Next<'a, State>) -> BoxFuture<'a, Response> {
         Box::pin(async move {
             let mut query_parameters: Option<HashMap<String, String>> = None;
@@ -115,16 +154,28 @@ impl<State> Middleware<State> for ValidatorMiddleware where State: Send + Sync +
                             let param_found: Result<String, _> = ctx.param(param_name);
                             if let Ok(param_value) = param_found {
                                 if let Err(err) = validator(&param_value[..]) {
-                                    return Response::new(400).body_string(format!("error on param '{}': {}", param_name, err));
+                                    return Response::new(400).body_json(&err).unwrap_or_else(
+                                        |err| {
+                                            return Response::new(500).body_string(format!(
+                                                "cannot serialize your parameter validator for '{}' error : {:?}",
+                                                param_name,
+                                                err
+                                            ));
+                                        },
+                                    );
                                 }
                             }
                         }
-                        
-                    },
+                    }
                     ParameterType::QueryParam(param_name) => {
                         if query_parameters.is_none() {
                             match ctx.query::<HashMap<String, String>>() {
-                                Err(err) => return Response::new(500).body_string(format!("cannot read query parameters: {:?}", err)),
+                                Err(err) => {
+                                    return Response::new(500).body_string(format!(
+                                        "cannot read query parameters: {:?}",
+                                        err
+                                    ))
+                                }
                                 Ok(qps) => query_parameters = Some(qps),
                             }
                         }
@@ -133,13 +184,20 @@ impl<State> Middleware<State> for ValidatorMiddleware where State: Send + Sync +
                         if let Some(qp_value) = query_parameters.get(&param_name[..]) {
                             for validator in validators {
                                 if let Err(err) = validator(qp_value) {
-                                    return Response::new(400).body_string(format!("error on param '{}': {}", param_name, err));
+                                    return Response::new(400).body_json(&err).unwrap_or_else(
+                                        |err| {
+                                            return Response::new(500).body_string(format!(
+                                                "cannot serialize your query parameter validator for '{}' error : {:?}",
+                                                param_name,
+                                                err
+                                            ));
+                                        },
+                                    );
                                 }
                             }
                         }
                     }
                 }
-                
             }
             next.run(ctx).await
         })
